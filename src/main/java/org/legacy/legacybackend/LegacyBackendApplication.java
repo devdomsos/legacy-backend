34df2c2bd5bf8e52c52beee9659966a328f4cbc3
devdomsos <devdomsos@gmail.com>
2024-04-08 15:12:56 +0200

    Initial commit

---
+++ src/main/java/org/legacy/legacybackend/LegacyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.legacy.legacybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/legacy/legacybackend/LegacyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.legacy.legacybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
